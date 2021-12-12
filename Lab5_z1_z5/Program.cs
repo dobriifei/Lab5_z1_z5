14d499a806f9549cc8bd4a81bb4acd2ac706a7d6
Vera <Vera@LAPTOP-2PS2I9DA>
2021-12-12 17:56:17 +0300

    Добавьте файлы проекта.

---
+++ Lab5_z1_z5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_z1_z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Сформировать массив из 7 элементов, вывести среднее арифметическое
+            Console.WriteLine("Введите 7 разных чисел");
+            int[] array = new int[7];
+            double summa = 0;
+            double srArifmeticheskoe;
+            {
+                for (int i = 0; i < 7; i++)
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < 7; i++)
+                    summa += array[i];
+                srArifmeticheskoe = summa / 7;
+            }
+            Console.WriteLine(" Сумма элементов равна {0}, а среднее арифметиеское элементов равно {1:f2}", summa, srArifmeticheskoe);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Z2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[N, N];
+            Console.WriteLine("Сформируем массив ");
+            for (int i = 0; i < N; i++)
+
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                            array[i, j] = 1;
+                        else
+                            array[i, j] = 0;
+                    }
+                    else
+                          if (j % 2 == 0)
+                        array[i, j] = 0;
+
+                    else
+                        array[i, j] = 1; 
+                    
+                    Console.Write(" {0 }", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
